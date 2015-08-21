24e22278da7a4aed4180d653cd8566b0307a71a3
park-byungsun <park.byungsun@linecorp.com>
2015-08-21 23:20:18 +0900

    화면이 너무 떨려서, remapCoordinateSystem()을 넣어 봤는데 이 문제는 아니고, 회전각을 처음 값과 비교하면 안 된다. 이전 값과 비교하도록 수정

--- app/src/main/java/com/example/lineplus/glcanvas/SensorController.java
+++ app/src/main/java/com/example/lineplus/glcanvas/SensorController.java
@@ -1,11 +1,14 @@
 package com.example.lineplus.glcanvas;
 
+import android.app.Activity;
 import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.util.Log;
+import android.view.Surface;
+import android.view.WindowManager;
 
 import com.squareup.otto.Bus;
 import com.squareup.otto.Subscribe;
@@ -13,16 +16,19 @@ import com.squareup.otto.Subscribe;
 public class SensorController implements SensorEventListener {
 	private static final int MIN_DEGREE = 5;
 	private final SensorManager mSensorManager;
+	private final WindowManager mWindowManager;
 	private final Bus eventBus;
 	private float[] mRotationM = new float[16];
 	private float[] mInitialRotationV = new float[3];
 	private float[] mPrevRotationM = new float[16];
 	private boolean needToSetInitialValue;
 
-	public SensorController(Context context, Bus eventBus) {
+	public SensorController(Activity activity, Bus eventBus) {
 		this.eventBus = eventBus;
 
-		mSensorManager = (SensorManager)context.getSystemService(Context.SENSOR_SERVICE);
+		mSensorManager = (SensorManager)activity.getSystemService(Context.SENSOR_SERVICE);
+		mWindowManager = activity.getWindow().getWindowManager();
+
 		eventBus.register(this);
 	}
 
@@ -48,6 +54,22 @@ public class SensorController implements SensorEventListener {
 			needToSetInitialValue = false;
 		}
 
+
+/*		int screenRotation = mWindowManager.getDefaultDisplay().getRotation();
+		switch (screenRotation) {
+			case Surface.ROTATION_0:
+			case Surface.ROTATION_180:
+				break;
+			case Surface.ROTATION_90:
+				SensorManager.remapCoordinateSystem(mRotationM, SensorManager.AXIS_Y, SensorManager.AXIS_MINUS_X, mRotationM);
+				break;
+			case Surface.ROTATION_270:
+				SensorManager.remapCoordinateSystem(mRotationM, SensorManager.AXIS_MINUS_Y, SensorManager.AXIS_X, mRotationM);
+				break;
+			default:
+				break;
+		}*/
+
 		float[] delta = new float[3];
 		SensorManager.getAngleChange(delta, mRotationM, mPrevRotationM);
 		toDegrees(delta);
@@ -59,21 +81,9 @@ public class SensorController implements SensorEventListener {
 
 			mPrevRotationM = mRotationM.clone();
 
-			float[] newAngles = new float[3];
-			newAngles[0] = degrees[0] - mInitialRotationV[0];
-			newAngles[1] = degrees[1] - mInitialRotationV[1];
-			newAngles[2] = degrees[2] - mInitialRotationV[2];
-			//			SensorManager.getOrientation(mRotationM, angles);
+			Log.i("new  ", String.format("%f, %f, %f", delta[0], delta[1], delta[2]));
 
-			//			toDegrees(angles);
-			Log.i("new  ", String.format("%f, %f, %f", newAngles[0], newAngles[1], newAngles[2]));
-			//			eventBus.post(new MainEvent.MatrixUpdated(mRotationM));
-
-//			z = 5;
-//			newAngles[0] = z;
-//			newAngles[1] = 0;
-//			newAngles[2] = 0;
-			eventBus.post(new MainEvent.MatrixUpdated(newAngles));
+			eventBus.post(new MainEvent.MatrixUpdated(delta));
 		}
 	}
 
